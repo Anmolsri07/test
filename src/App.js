8e4bdbc125e65975491c1befe69e9d1c85efde3b
Anmolsri07 <anmolsri001.dmk@gmail.com>
2024-02-10 09:06:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Home from "./Home";
+import { Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
+  const [value, setValue] = useState(0);
+  let arr = ["a", "b"];
+  useEffect(() => {
+    console.log("runing");
+    arr.push("qwertyui");
+
+    setValue((prev) => {
+      return prev + 1;
+    });
+  }, []);
+
+  console.log(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link to="home">Home</Link>
+      <Routes>
+        <Route path="home" element={<Home title="Anmol" />} />
+      </Routes>
+    </>
   );
 }
 
